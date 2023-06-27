b9a6367d601dce52b7f7458c01c8c02c9e66f271
Atin Gupta <atingupta@email.com>
2023-06-27 09:04:23 +0530

    -

--- Hands-on/4-Azure-Terraform/06-Input-Variables-Basic/terraform-manifests/c3-resource-group.tf
+++ Hands-on/4-Azure-Terraform/06-Input-Variables-Basic/terraform-manifests/c3-resource-group.tf
@@ -3,5 +3,6 @@ resource "azurerm_resource_group" "myrg" {
   #name = "my-rg1" 
   #name = var.resource_group_name
   name = "${var.business_unit}-${var.environment}-${var.resource_group_name}"
+  #name = "it-dev-myrg"
   location = var.resource_group_location
 }
---
+++ Hands-on/4-Azure-Terraform/13-Terraform-Remote-State-Storage-and-Locking/terraform-manifests/commands.sh
@@ -0,0 +1,18 @@
+# get terraform version
+terraform version
+# Terraform v0.13.5
+
+# get terraform commands
+terraform
+
+# init terraform's Azure provider (main.tf)
+terraform init
+
+# plan and preview terraform changes
+terraform plan
+
+# deploy terraform infra
+terraform apply
+
+# destroy infra
+terraform destroy
