496f4ee06a350108731b464fd9e13b31489d8dbf
Atin Gupta <atingupta@email.com>
2023-07-01 18:48:03 +0530

    -

--- Hands-on/5-Ansible/14-misc-topics/commands.sh
+++ Hands-on/5-Ansible/14-misc-topics/commands.sh
@@ -1,16 +1,16 @@
-Passing multiple inventory sources
+cd ~/sre-fundamentals-may-23/Hands-on/5-Ansible/14-misc-topics
+
+# Passing multiple inventory sources
 ansible-playbook 24-Print-Variable-Value.yml -i staging -i production --list-hosts
 
-ansible-playbook 24-Print-Variable-Value.yml -i staging alpha.example.org --list-hosts
+ansible-playbook 24-Print-Variable-Value.yml -i staging -l alpha.example.org:beta.example.org --list-hosts
 
-ansible-playbook 24-Print-Variable-Value.yml -i staging alpha.example.org:beta.example.org --list-hosts
+ansible-playbook 24-Print-Variable-Value.yml -i staging -l webservers --list-hosts
 
-ansible-playbook 24-Print-Variable-Value.yml -i staging webservers --list-hosts
-
-ansible-playbook 24-Print-Variable-Value.yml -i staging webservers:dbservers --list-hosts
+ansible-playbook 24-Print-Variable-Value.yml -i staging -l webservers:dbservers --list-hosts
 
 #Only servers that are both in webservers and dbservers
-ansible-playbook 24-Print-Variable-Value.yml -i staging webservers:&dbservers --list-hosts
+ansible-playbook 24-Print-Variable-Value.yml -i staging -l webservers:\&dbservers --list-hosts
 
 # Servers in webservers except those also in dbservers
-ansible-playbook 24-Print-Variable-Value.yml -i staging webservers:!dbservers --list-hosts
+ansible-playbook 24-Print-Variable-Value.yml -i staging -l webservers:\!dbservers --list-hosts
