86255d1bead260c3397f908d8efbbb4afdd25b88
Atin Gupta <atingupta@email.com>
2023-07-01 14:13:41 +0530

    -

---
+++ Hands-on/5-Ansible/13-misc-modules/commands.sh
@@ -0,0 +1,7 @@
+# Create Docker containers
+bash ~/sre-fundamentals-may-23/Hands-on/5-Ansible/recreate-docker-containers.sh
+bash ~/sre-fundamentals-may-23/Hands-on/5-Ansible/start-docker-containers.sh
+
+cd ~/sre-fundamentals-may-23/Hands-on/5-Ansible/13-misc-modules
+
+ansible-playbook misc-modules.yaml
