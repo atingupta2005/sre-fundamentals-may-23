79913d65ab9acfb52790e4f70f5dc14ca4b0cb92
Atin Gupta <atingupta@email.com>
2023-07-01 19:01:24 +0530

    -

--- Hands-on/5-Ansible/11-ansible-roles/commands.sh
+++ Hands-on/5-Ansible/11-ansible-roles/commands.sh
@@ -12,7 +12,7 @@ cd ~/sre-fundamentals-may-23/Hands-on/5-Ansible/11-ansible-roles
 ## - Create ansible.cfg in current directory
   ## Already done
 ls ansible.cfg
-  
+
 ## - Provide path (roles_path) to roles directory in ansible.cfg
   ## Already done
 cat ansible.cfg | grep roles_path
@@ -26,32 +26,19 @@ ansible-galaxy init roles/update_repos --offline
 tree roles/update_repos
 
 ## Copy task (update_repos) from install_and_configure_tomcat.yml
-```
- - name: Updating repos
-   yum:
-     name: "*"
-     state: latest
-```
-
-```
----
- - name: using roles
-   hosts: all
-   gather_facts: false
-   become: yes
-   roles:
-    - update_repos
-```
-
-ansible-playbook install-and-configure-tomcat-java.yml
+cat > roles/update_repos/tasks/main.yml << EOL
+- name: Updating repos
+  yum:
+    name: "*"
+    state: latest
+EOL
 
 ### Create Role to install and configure java path
 ansible-galaxy init roles/xyz_java --offline
-nano roles/xyz_java/tasks/main.yml
 
-```
+cat > roles/xyz_java/tasks/main.yml << EOL
 - name: Installing required java
-  yum:
+  apt:
     name: "{{req_java}}"
     state: present
 - name: Setting default java
@@ -59,38 +46,34 @@ nano roles/xyz_java/tasks/main.yml
     name: java
     link: /usr/bin/java
     path: /usr/lib/jvm/{{set_java}}/bin/java
-```
+EOL
 
- - nano roles/xyz_java/vars/main.yml
+cat > roles/xyz_java/vars/main.yml << EOL
+req_java: java-1.8.0-openjdk
+set_java: jre-1.8.0-openjdk
+EOL
 
-```
- req_java: java-1.8.0-openjdk
- set_java: jre-1.8.0-openjdk
-```
-
- - nano install-and-configure-tomcat-java.yml    #Its the main playbook which will use roles
-
-```
----
- - name: using roles
-   hosts: all
-   gather_facts: false
-   become: yes
-   roles:
+rm install-and-configure-tomcat-java.yml
+cat > install-and-configure-tomcat-java.yml << EOL
+- name: using roles
+  hosts: all
+  gather_facts: false
+  become: yes
+  roles:
     - update_repos
     - xyz_java
-```
+    - xyz_tomcat
 
 ### Create Role to download and install tomcat
- - ansible-galaxy init roles/xyz_tomcat
- - nano roles/xyz_tomcat/vars/main.yml
+- ansible-galaxy init roles/xyz_tomcat
+- nano roles/xyz_tomcat/vars/main.yml
 
 ```
- req_tomcat_ver: 9.0.41
- tomcat_url: http://mirrors.estointernet.in/apache/tomcat/tomcat-{{req_tomcat_ver.split('.')[0]}}/v{{req_tomcat_ver}}/bin/apache-tomcat-{{req_tomcat_ver}}.tar.gz
- tomcat_port: 8090
+req_tomcat_ver: 9.0.41
+tomcat_url: http://mirrors.estointernet.in/apache/tomcat/tomcat-{{req_tomcat_ver.split('.')[0]}}/v{{req_tomcat_ver}}/bin/apache-tomcat-{{req_tomcat_ver}}.tar.gz
+tomcat_port: 8090
 ```
- - Copy server.xml.j2 to templates directory
+- Copy server.xml.j2 to templates directory
 
 ```
 
@@ -98,7 +81,7 @@ cp server.xml.j2 roles/xyz_tomcat/templates
 
 ```
 
- - nano roles/xyz_tomcat/tasks/main.yml
+- nano roles/xyz_tomcat/tasks/main.yml
 
 ```
 - name: Downloading required tomcat
@@ -118,22 +101,22 @@ cp server.xml.j2 roles/xyz_tomcat/templates
     dest: /usr/local/latest/conf/server.xml
 ```
 
- - nano install-and-configure-tomcat-java.yml    #Its the main playbook which will use roles
+- nano install-and-configure-tomcat-java.yml    #Its the main playbook which will use roles
 
 ```all
 ---
- - name: using roles
-   hosts: all
-   gather_facts: false
-   become: yes
-   roles:
+- name: using roles
+  hosts: all
+  gather_facts: false
+  become: yes
+  roles:
     - update_repos
     - xyz_java
     - xyz_tomcat
 ```
 
 ### Setup handlers to start tomcat
- - nano roles/xyz_tomcat/handlers/main.yml
+- nano roles/xyz_tomcat/handlers/main.yml
 
 ```
 - name: start_tomcat
@@ -163,11 +146,11 @@ cp server.xml.j2 roles/xyz_tomcat/templates
 ```
 
 ### Run the playbook
- - ansible-playbook install-and-configure-tomcat-java.yml
- - curl localhost:8090
+- ansible-playbook install-and-configure-tomcat-java.yml
+- curl localhost:8090
 
 
 ### Creating Custom Ansible Roles
- - Do you need a role, but you can’t find it in Ansible Galaxy? Follow our guide to creating your own.
+- Do you need a role, but you can’t find it in Ansible Galaxy? Follow our guide to creating your own.
 
 [Creating Custom Ansible Roles](https://azurecitadel.com/automation/packeransible/lab5/)
