051ca95e0133111ca71ee491618c22529a615577
Atin Gupta <atingupta@email.com>
2023-07-05 10:24:07 +0530

    -

--- Hands-on/5-Ansible/12-misc-modules/commands.sh
+++ Hands-on/5-Ansible/12-misc-modules/commands.sh
@@ -2,6 +2,11 @@
 bash ~/sre-fundamentals-may-23/Hands-on/5-Ansible/recreate-docker-containers.sh
 bash ~/sre-fundamentals-may-23/Hands-on/5-Ansible/start-docker-containers.sh
 
-cd ~/sre-fundamentals-may-23/Hands-on/5-Ansible/13-misc-modules
+cd ~/sre-fundamentals-may-23/Hands-on/5-Ansible/12-misc-modules
 
-ansible-playbook misc-modules.yaml
+ansible-playbook misc-modules-1.yaml
+
+ansible-playbook misc-modules-2.yaml
+ansible-playbook run-local-script-on-remote.yaml
+ansible-playbook seboolean.yaml
+ansible-playbook selinux.yaml
