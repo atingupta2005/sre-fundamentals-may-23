04b329b62c53e9223fea12cbc38fb45613197685
Atin Gupta <atingupta@email.com>
2023-07-05 09:03:05 +0530

    -

---
+++ Hands-on/04-Ansible/14-windows/commands.sh
@@ -0,0 +1,12 @@
+cd ~/sre-fundamentals-may-23/Hands-on/5-Ansible/12-windows
+
+ansible-playbook 1-win_ping.yml
+ansible-playbook 2-file-windows.yml
+ansible-playbook 3-file_exist_windows.yml
+ansible-playbook 4-win_chocolatey.yml
+ansible-playbook 5-win_copy.yml
+ansible-playbook 6-win_get_url.yml
+ansible-playbook 7-win_file.yml
+ansible-playbook 8-directory_exists_windows.yml
+ansible-playbook 9-printdotnet.yml
+ansible-playbook 10-win_reboot.yml
